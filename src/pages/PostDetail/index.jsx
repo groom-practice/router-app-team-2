50039a1b0042a41d1d7c24b03ea92ad0668df079
SNXWXH <snxwxh@gmail.com>
2025-04-21 12:16:44 +0900

    fix: 오타 수정

--- src/pages/PostDetail/index.jsx
+++ src/pages/PostDetail/index.jsx
@@ -1,21 +1,21 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams } from 'react-router-dom';
 
-import { getIdPost } from "../../apis/posts";
-import { useEffect, useState } from "react";
-import "../../App.css";
+import { getPostById } from '../../apis/posts';
+import { useEffect, useState } from 'react';
+import '../../App.css';
 
 export default function PostDetail() {
   const { id } = useParams();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
-    getIdPost(id).then((res) => setPost(res));
+    getPostById(id).then((res) => setPost(res));
   }, [id]);
 
   if (!post) return <div>...Loading</div>;
 
   return (
-    <div className="postDetailDiv">
+    <div className='postDetailDiv'>
       <h1>Post id : {id}</h1>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,18 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 import MainLayout from '../layouts/MainLayout';
-import PostDetail from "../pages/PostDetail";
+import PostDetail from '../pages/PostDetail';
 import EditPost from '../pages/EditPost';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <MainLayout />,
-    childeren: [
+    children: [
       { index: true, element: <Home /> },
       // { path: "posts", element: <PostList /> },
-      { path: "posts/:id", element: <PostDetail /> },
-      { path: "posts/:id/edit", element: <EditPost /> },
+      { path: 'posts/:id', element: <PostDetail /> },
+      { path: 'posts/:id/edit', element: <EditPost /> },
     ],
   },
 ]);
