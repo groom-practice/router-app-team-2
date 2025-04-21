3edf7ca366d42df5131a947d90fbde93432a3d19
구름 <dong21co@naver.com>
2025-04-21 11:24:27 +0900

    home_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';  // 홈 페이지 컴포넌트 임포트
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>리액트 앱</h1>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />  {/* 홈 페이지 라우팅 */}
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/routes/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Home from '../pages/Home';
+
+import MainLayout from '../layouts/MainLayout';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+       { index: true, element: <Home /> },
+    ],
+  },
+]);
+
+export default router;
