7b249fd3ccc4f0820fb6f1722a621dd0978c8d24
김소연 <ksy9252@naver.com>
2025-04-22 11:40:00 +0900

    Fix : router 수정

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,19 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Home from '../pages/Home';
-import MainLayout from '../layouts/MainLayout';
-// import PostList from '../pages/PostList';
-import PostDetail from '../pages/PostDetail';
-import EditPost from '../pages/EditPost';
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayout from "../layouts/MainLayout";
+import PostList from "../pages/PostList";
+import PostDetail from "../pages/PostDetail";
+import EditPost from "../pages/EditPost";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
       { index: true, element: <Home /> },
-      // { path: "posts", element: <PostList /> },
-      { path: 'posts/:id', element: <PostDetail /> },
-      { path: 'posts/:id/edit', element: <EditPost /> },
+      { path: "posts", element: <PostList /> },
+      { path: "posts/:id", element: <PostDetail /> },
+      { path: "posts/:id/edit", element: <EditPost /> },
     ],
   },
 ]);
