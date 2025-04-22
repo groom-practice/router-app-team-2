d683f4b76b5659f214459e6daa82e6fd797a6736
구름 <dong21co@naver.com>
2025-04-22 11:48:58 +0900

    수정

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 import MainLayout from '../layouts/MainLayout';
-// import PostList from '../pages/PostList';
+import PostList from '../pages/PostList';
 import PostDetail from '../pages/PostDetail';
 import EditPost from '../pages/EditPost';
+
 
 const router = createBrowserRouter([
   {
@@ -11,7 +12,7 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { index: true, element: <Home /> },
-      // { path: "posts", element: <PostList /> },
+      { path: "posts", element: <PostList /> },
       { path: 'posts/:id', element: <PostDetail /> },
       { path: 'posts/:id/edit', element: <EditPost /> },
     ],
